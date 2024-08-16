eb2a570d22376d299087541d521ac7b5c849ecd6
hampus <hampus.hauffman@trustly.com>
2024-08-16 23:08:46 +0200

    wow so cool

--- script/base_player.gd
+++ script/base_player.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
+const SPEED: float         = 300.0
+const JUMP_VELOCITY: float = -400.0
+@onready var player_number: int = $PlayerNumber.player_number
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+var current_player_number: int = PlayerNumber.current_player
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+    if player_number == current_player_number:
+        move(delta)
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func move(delta: float) -> void:
+    # Add the gravity.
+    if not is_on_floor():
+        velocity += get_gravity() * delta
 
-	move_and_slide()
+    # Handle jump.
+    if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+        velocity.y = JUMP_VELOCITY
+
+    # Get the input direction and handle the movement/deceleration.
+    # As good practice, you should replace UI actions with custom gameplay actions.
+    var direction := Input.get_axis("ui_left", "ui_right")
+    if direction:
+        velocity.x = direction * SPEED
+    else:
+        velocity.x = move_toward(velocity.x, 0, SPEED)
+
+    move_and_slide()
---
+++ script/player_number.gd
@@ -0,0 +1,14 @@
+extends Node
+class_name PlayerNumber
+
+@export var player_number: int = 1
+static var current_player: int = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
