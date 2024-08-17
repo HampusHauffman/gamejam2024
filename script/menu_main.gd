4c55ee66de7baa405b503d7fd141acc128a52520
ocwest <106112443+ocwest@users.noreply.github.com>
2024-08-17 18:52:37 +0200

    base player sprites and collision (#14)
    Added sprites for base player and added collsion as scenes

---
+++ script/menu_main.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+
+func _on_play_pressed():
+	print("Play")
+	$VBoxContainer/Play/click_sound.playing = true
+	$VBoxContainer/Play/animation_play_pressed.play()
+	$scene_delay.start()
+	
+	
+func _on_quit_pressed():
+	print("Quit")
+	get_tree().quit()
+
+
+func _on_play_mouse_entered():
+	$VBoxContainer/Play/hover_sound_play.play()
+	
+
+func _on_tree_entered():
+	$menu_main_song.play()
+
+
+func _on_scene_delay_timeout():
+	get_tree().change_scene_to_file("res://scene/main.tscn")
