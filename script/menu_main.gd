3c83f825451c88acc5cd4e1f391d710a6c259e4d
ocwest <oscarwesterblom@gmail.com>
2024-08-17 22:21:00 +0200

    main menu

--- script/menu_main.gd
+++ script/menu_main.gd
@@ -7,11 +7,17 @@ extends Control
 
 func _on_play_pressed():
 	print("Play")
+<<<<<<< Updated upstream
 	$VBoxContainer/Play/click_sound.playing = true
 	$VBoxContainer/Play/animation_play_pressed.play()
 	$scene_delay.start()
 	
 	
+=======
+	get_tree().change_scene_to_file("res://scene/main.tscn")
+	
+
+>>>>>>> Stashed changes
 func _on_quit_pressed():
 	print("Quit")
 	get_tree().quit()
@@ -19,6 +25,7 @@ func _on_quit_pressed():
 
 func _on_play_mouse_entered():
 	$VBoxContainer/Play/hover_sound_play.play()
+<<<<<<< Updated upstream
 	
 
 func _on_tree_entered():
@@ -27,3 +34,9 @@ func _on_tree_entered():
 
 func _on_scene_delay_timeout():
 	get_tree().change_scene_to_file("res://scene/main.tscn")
+=======
+
+
+func _on_tree_entered():
+	$menu_main_song.play()
+>>>>>>> Stashed changes
