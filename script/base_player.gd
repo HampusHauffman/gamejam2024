4f166a3bed169c6b626993e46507a9d0a72e8550
meyercode <adam@meyerfam.com>
2024-08-17 12:17:05 +0200

    use move_and_collide x and y for moving and scale with zoom

--- script/base_player.gd
+++ script/base_player.gd
@@ -5,14 +5,16 @@ const SPEED: float         = 300.0
 const JUMP_VELOCITY: float = -400.0
 # This is set by the game manager.
 var isActive: bool = false
+var is_on_ground = false
+
 
 @export var zoom: float = 1.0
 
 
 func set_is_active(active: bool) -> void:
 	isActive = active
-	if not active:
-		velocity.x = 0  # TODO Fix this bug. currently we continue moving right if we deactivate the player while moving right.
+	#if not active:
+	#	velocity.x = 0  # TODO Fix this bug. currently we continue moving right if we deactivate the player while moving right.
 
 
 func _physics_process(delta: float) -> void:
@@ -20,18 +22,35 @@ func _physics_process(delta: float) -> void:
 		handle_input(delta)
 
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_ground:
 		velocity += get_gravity() * delta
-	move_and_slide()
+		
+	var horizontal_velocity = Vector2(velocity.x, 0)
+	var collision = move_and_collide(horizontal_velocity * delta * zoom/2)
+	if collision:
+		var slide_factor = 1  # Adjust this value to control sliding
+		horizontal_velocity = horizontal_velocity.slide(collision.get_normal()) * slide_factor
+		velocity.x = horizontal_velocity.x
 
+	# Handle vertical movement
+	collision = move_and_collide(Vector2(0, velocity.y * delta * zoom/2))
+	if collision:
+		if collision.get_normal().dot(Vector2.UP) > 0.7:  # Adjust this value as needed
+			is_on_ground = true
+		else:
+			is_on_ground = false
+			velocity.y = 0
+	else:
+		is_on_ground = false
 
 func handle_input(delta: float) -> void:
 	if not isActive:
 		return
 
 	# Handle jump.
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_ground:
 		velocity.y = JUMP_VELOCITY
+		is_on_ground = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
